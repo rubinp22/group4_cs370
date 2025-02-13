2cf20080cab4330dd1b59461c7f5fd662c56dea3
NoJStein <127149391+NoJStein@users.noreply.github.com>
2025-02-13 15:04:37 -0800

    Weights and Running Graphs added to new branch
    I have just made a new branch, which is a copy of our main branch at its most recent state. At this point, my most recent graphs were overwritten by my old graphs, so I am updating them here. When I'm happy with the graphs, I will do a pull request and merge this code with our main branch.

--- src/components/RunningMetrics.jsx
+++ src/components/RunningMetrics.jsx
@@ -2,19 +2,154 @@ import { useNavigate } from 'react-router-dom';
 import { Stack, Card, Typography } from '@mui/material';
 import { BarChart } from '@mui/x-charts';
 
+const maxMETs = [10, 14, 18];
+const restingHeartRates = [100, 70, 50]
+
+const runningData = [
+    {
+        distance: 1.5,      // Distance ran in miles
+        duration: 0.25,      // hours
+        steps: 2500,
+        avgHeartRate: 125,
+        maxHeartRate: 145,
+        bodyWeight: 70,     // in Kg, equal to 154 lbs
+        fitnessLevel: 0
+    },
+    {
+        distance: 2,      
+        duration: 0.3,      
+        steps: 3200,
+        avgHeartRate: 130,
+        maxHeartRate: 150,
+        bodyWeight: 70,     
+        fitnessLevel: 0
+    },
+    {
+        distance: 2.5,      
+        duration: 0.35,      
+        steps: 4000,
+        avgHeartRate: 130,
+        maxHeartRate: 155,
+        bodyWeight: 70,     
+        fitnessLevel: 1
+    },
+    {
+        distance: 4,      
+        duration: 0.5,      
+        steps: 5500,
+        avgHeartRate: 125,
+        maxHeartRate: 145,
+        bodyWeight: 70,     
+        fitnessLevel: 1
+    },
+    {
+        distance: 1.5,      
+        duration: 0.5,      
+        steps: 4500,
+        avgHeartRate: 125,
+        maxHeartRate: 145,
+        bodyWeight: 70,     
+        fitnessLevel: 0
+    }
+]
+
+const distance = runningData.map(data => data.distance);
+const duration = runningData.map(data => data.duration);
+const steps = runningData.map(data => data.steps);
+const avgHeartRate = runningData.map(data => data.avgHeartRate);
+const maxHeartRate = runningData.map(data => data.maxHeartRate);
+const bodyWeight = runningData.map(data => data.bodyWeight);
+const fitnessLevel = runningData.map(data => data.fitnessLevel);
+
+// speed = distance / duration
+const speed = distance.map((data, index) => data / duration[index]);
+// pace = duration / distance
+// How long does it take to run a mile (or km if we decide to support that unit)
+const pace = duration.map((data, index) => data / distance[index]);
+// cadence = steps / minutes
+// Also known as steps per minute (SPM). Lower cadences (<160 SPM) indicates longer strides, and
+// more impact per step. This increases injury risk for beginners, so it is safer and more efficient
+// to have a cadence of 170 - 190 SPM
+const cadence = steps.map((data, index) => data / (duration[index] * 60));
+
+const heartRateReserve = avgHeartRate.map((data, index) => 
+    (data - restingHeartRates[fitnessLevel[index]]) / (maxHeartRate[index] - restingHeartRates[fitnessLevel[index]]));
+
+const MET = heartRateReserve.map((data, index) => (data * (maxMETs[fitnessLevel[index]] - 1) + 1))
+
+const caloriesBurned = MET.map((data, index) => parseInt(data * duration[index] * bodyWeight[index]));
+
+const labels = runningData.map((data, index) => `run ${index + 1}`)
+const graphMargin = 3;
+
 function RunningMetrics() {
     const navigate = useNavigate();
+    return (
+        <Stack>
+            <Typography fontSize={32}>
+                Running Metrics
+            </Typography>
+            <Stack direction="row">
+                <Card sx={{ margin: graphMargin, backgroundColor: "#828c85",}}>
+                    <BarChart
+                        xAxis={[{ scaleType: "band", data: labels }]}
+                        series={[{ data: distance, label: "Distance Ran (Miles)" }]}
+                        width={500}
+                        height={300}
+                    />
+                </Card>
+                <Card sx={{ margin: graphMargin, backgroundColor: "#828c85",}}>
+                    <BarChart
+                        xAxis={[{ scaleType: "band", data: labels }]}
+                        series={[{ data: speed, label: "Running Speed (MPH)" }]}
+                        width={500}
+                        height={300}
+                    />
+                </Card>
+            </Stack>
+            <Stack direction="row">
+                <Card sx={{ margin: graphMargin, backgroundColor: "#828c85",}}>
+                    <BarChart
+                        xAxis={[{ scaleType: "band", data: labels }]}
+                        series={[{ data: pace, label: "Time per mile (in hours)" }]}
+                        width={500}
+                        height={300}
+                    />
+                </Card>
+                <Card sx={{ margin: graphMargin, backgroundColor: "#828c85",}}>
+                    <BarChart
+                        xAxis={[{ scaleType: "band", data: labels }]}
+                        series={[{ data: cadence, label: "Cadence (Steps per minute)" }]}
+                        width={500}
+                        height={300}
+                    />
+                </Card>
+            </Stack>
+            <Stack direction="row">
+                <Card sx={{ margin: graphMargin, backgroundColor: "#828c85",}}>
+                    <BarChart
+                        xAxis={[{ scaleType: "band", data: labels }]}
+                        series={[
+                            {data: avgHeartRate, label: "Avg Heart Rate" },
+                            {data: maxHeartRate, label: "Max Heart Rate" }
+                        ]}
+                        width={500}
+                        height={300}
+                    />
+                </Card>
+                <Card sx={{ margin: graphMargin, backgroundColor: "#828c85",}}>
+                    <BarChart
+                        xAxis={[{ scaleType: "band", data: labels }]}
+                        series={[{ data: caloriesBurned, label: "Estimated Calories Burned" }]}
+                        width={500}
+                        height={300}
+                    />
+                </Card>
+            </Stack>
+            <button onClick={() => navigate('/fitnessTypes')}>Back to Fitness Types</button>
+        </Stack>
 
-
-return (
-    <div>
-      <h2>RUNNING METRICS</h2>
-      <p>Log your runs, track your pace, and set new goals.</p>
-
-      <button onClick={() => navigate('/fitnessTypes')}>Back to Fitness Types</button>
-    </div>
-  );
-
+    );
 }
 
-export default RunningMetrics 
+export default RunningMetrics
--- src/components/WeightLiftingMetrics.jsx
+++ src/components/WeightLiftingMetrics.jsx
@@ -6,14 +6,23 @@ import { BarChart } from '@mui/x-charts';
 
 // MET (Metabolic Equivalent of Task) is defined as the energy expenditure for a given task
 // An MET of 1 is measured as the energy expenditure at rest. 
-// If you weigh 70 Kg (154 lbs), you burn 70 calories per hour (kcal/hr)
+// If you weigh 70 Kg (154 lbs), you burn 70 calories per hour (kcal/hr) at 1 MET
 // Depending on your fitness level, your heart will have a cap on how much oxygen it can absorb. There are many
 // other factors, but if you are out of shape, your max MET will be lower than if you were a highly trained athelete.
 // These values will be used to estimate the calories burned in any given exercise. 
-let maxMETs = [10, 14, 18];
+// 0 - Sedentary Individuals: 10
+// 1 - Average Fitness: 14
+// 2 - Highly Trained Atheletes: 18
+const maxMETs = [10, 14, 18];
+
+// Resting Heart Rates by fitness levels:
+// 0 - Sedentary Individuals: 100
+// 1 - Average Fitness: 70
+// 2 - Highly Trained Atheletes: 50
+const restingHeartRates = [100, 70, 50]
 
 // Imagine this data represents someone who is continually improving at weight lifting
-let weightLiftData = [
+const weightLiftData = [
     {
         reps: 15,
         sets: 2,
@@ -72,39 +81,124 @@ let weightLiftData = [
 ]
 // Eventually we should allow users to input this data, but I want to keep things simple for now
 
-
-function WeightLiftingMetrics () {
+function WeightLiftingMetrics() {
     const navigate = useNavigate();
 
-    return (
-        <>
-            {/* Card and BarChart are MUI components. They each have an attribute called sx which can be used
-            to add additional styling */}
-            <Card sx={{ marginBottom: 5, backgroundColor: "#828c85",}}>
-                <BarChart
-                    xAxis={[{ scaleType: 'band', 
-                        data: 
-                            [
-                                'Reps over Time', 
-                                'Sets over Time', 
-                                'Weight over Time'
-                            ] 
-                    }]}
-                    series={[
-                        { data: [weightLiftData[0].reps, weightLiftData[0].sets, weightLiftData[0].weightOfWeights] }, 
-                        { data: [weightLiftData[1].reps, weightLiftData[1].sets, weightLiftData[1].weightOfWeights] }, 
-                        { data: [weightLiftData[2].reps, weightLiftData[2].sets, weightLiftData[2].weightOfWeights] },
-                        { data: [weightLiftData[3].reps, weightLiftData[3].sets, weightLiftData[3].weightOfWeights] },
-                        { data: [weightLiftData[4].reps, weightLiftData[4].sets, weightLiftData[4].weightOfWeights] }
-                    ]}
-                    width={700}
-                    height={300}
-                />
-            </Card>
+    // Pulling individual data from the objects in weightLiftData into their own arrays
+    const reps = weightLiftData.map(data => data.reps);
+    const sets = weightLiftData.map(data => data.sets);
+    const weightOfWeights = weightLiftData.map(data => data.weightOfWeights);
+    const bodyWeight = weightLiftData.map(data => data.bodyWeight);
+    const avgHeartRate = weightLiftData.map(data => data.avgHeartRate);
+    const maxHeartRate = weightLiftData.map(data => data.maxHeartRate);
+    const fitnessLevel = weightLiftData.map(data => data.fitnessLevel);
+    const duration = weightLiftData.map(data => data.duration);
 
+    // totalReps = reps * sets
+    const totalReps = reps.map((data, index) => data * sets[index]);
+    // totalVolume = totalReps * weightofWeights        This represents the total weight lifted during a session
+    const totalVolume = totalReps.map((data, index) => data * weightOfWeights[index]);
+
+    // Uses Epley's formula to determine your theoretical maximum weight for a lift without you attempting a dangerous lift
+    // Useful for tracking progress over time
+    // Epley's formula: weightOfWeights * (1 + (totalReps / 30))
+    const maxRep = weightOfWeights.map((data, index) => data * (1 + (totalReps[index] / 30)));
+
+    // strengthRatio = maxRep / bodyWeight
+    // Normalizes strength performance relative to body weight
+    // A strength ratio of 0.5 means you can lift half of your body weight, while a ratio of 1 or more means you can lift
+    // your body weight or more (like an ant 🐜)
+    const strengthRatio = maxRep.map((data, index) => data / bodyWeight[index]);
+
+    // heartRateReserve = ((avgHeartRate - restingHeartRate) / (maxHeartRate - restingHeartRate))
+    // Expresses how much of your heart's available capacity you are using during exercise
+    // A value of 50% indicates you were exercising at 50% of your heart rate reserve
+    // Important for calculating the estimated calories burnt during a workout.
+    const heartRateReserve = avgHeartRate.map((data, index) => 
+        (data - restingHeartRates[fitnessLevel[index]]) / (maxHeartRate[index] - restingHeartRates[fitnessLevel[index]]));
+
+    // MET = (heartRateReserve * (maxMET - 1)) + 1
+    // MET was explained earlier in the documentation, it is crucial for estimating calories burnt
+    const MET = heartRateReserve.map((data, index) => (data * (maxMETs[fitnessLevel[index]] - 1) + 1))
+    
+    // caloriesBurned = MET * duration(hours) * bodyWeight(kg)
+    const caloriesBurned = MET.map((data, index) => parseInt(data * duration[index] * bodyWeight[index]));
+
+    const labels = weightLiftData.map((_, index) => `Session ${index + 1}`);
+
+    const graphMargin = 3;
+
+    // Stacks are MUI components that quickly let you stack nested components horizontally and vertically instead of 
+    //      manually needing to define flex boxes through CSS
+    // Cards are useful as backdrops to make other components stand out
+    // Typography is an alternative component to any built in HTML text component (like H1 or P)
+    //      Side Note: Page scrapers parsing through HTML will often use H1 components to "sum up" what a web page
+    //      represents. If you use H1's through H6's a lot, you may confuse search engines. But we don't have to 
+    //      worry about that.
+    return (
+        <Stack>
+            <Typography fontSize={32}>
+                Weight Lifting Metrics
+            </Typography>
+            <Stack direction="row">
+                <Card sx={{ margin: graphMargin, backgroundColor: "#828c85",}}>
+                    <BarChart
+                        xAxis={[{ scaleType: "band", data: labels }]}
+                        series={[{ data: totalReps, label: "Total Reps" }]}
+                        width={500}
+                        height={300}
+                    />
+                </Card>
+                <Card sx={{ margin: graphMargin, backgroundColor: "#828c85",}}>
+                <BarChart
+                        xAxis={[{ scaleType: "band", data: labels }]}
+                        series={[{ data: totalVolume , label: "Total Volume (Kg)" }]}
+                        width={500}
+                        height={300}
+                    />
+                </Card>
+            </Stack>
+            <Stack direction="row">
+                <Card sx={{ margin: graphMargin, backgroundColor: "#828c85",}}>
+                    <BarChart
+                        xAxis={[{ scaleType: "band", data: labels }]}
+                        series={[{ data: maxRep, label: "1 Rep Max (Kg)" }]}
+                        width={500}
+                        height={300}
+                    />
+                </Card>
+                <Card sx={{ margin: graphMargin, backgroundColor: "#828c85",}}>
+                    <BarChart
+                        xAxis={[{ scaleType: "band", data: labels }]}
+                        series={[{ data: strengthRatio, label: "Strength to Weight Ratio" }]}
+                        width={500}
+                        height={300}
+                    />
+                </Card>
+            </Stack>
+            <Stack direction="row">
+                <Card sx={{ margin: graphMargin, backgroundColor: "#828c85",}}>
+                    <BarChart
+                        xAxis={[{ scaleType: "band", data: labels }]}
+                        series={[
+                            {data: avgHeartRate, label: "Avg Heart Rate" },
+                            {data: maxHeartRate, label: "Max Heart Rate" }
+                        ]}
+                        width={500}
+                        height={300}
+                    />
+                </Card>
+                <Card sx={{ margin: graphMargin, backgroundColor: "#828c85",}}>
+                    <BarChart
+                        xAxis={[{ scaleType: "band", data: labels }]}
+                        series={[{ data: caloriesBurned, label: "Estimated Calories Burned" }]}
+                        width={500}
+                        height={300}
+                    />
+                </Card>
+            </Stack>
             <button onClick={() => navigate('/fitnessTypes')}>Back to Fitness Types</button>
-        </>
-        
+        </Stack>
 
     );
 }
