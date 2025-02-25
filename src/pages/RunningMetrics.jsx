6b308bb3d0cc8fe9318bdcb21e949d89d4a36849
NoJStein <127149391+NoJStein@users.noreply.github.com>
2025-02-24 16:38:05 -0800

    Inputting Running Metrics now works
    Added the ability for the user to open a menu full of text fields where they can input all relevant metrics for a given run. The graphs above will update as the user inputs more data into them.
    
    The implementation for this maybe could be slimmed down eventually, but here's a basic explanation for how it works. I re-declared the array of objects called runningData as state, so that changes in its value would appear on screen without refreshing the page. Each new text field is linked to its corresponding state so that their values can update in the same way. Finally, clicking the Submit button pushes a new object with the appropriate state into runningData. From there the graphs populate with their data.
    
    There is currently a mathematical bug where you can burn negative calories in your exercises. As a user, I would be cross if I went for a run and my body thought I ate a piece of chocolate cake instead. Needless to say, I will be looking into this very soon!

--- src/pages/RunningMetrics.jsx
+++ src/pages/RunningMetrics.jsx
@@ -1,88 +1,78 @@
 import { Link } from 'react-router-dom';
-import { Stack, Card, Typography } from '@mui/material';
+import { Stack, Card, Typography, Button, TextField, InputAdornment } from '@mui/material';
 import { BarChart } from '@mui/x-charts';
+import { useState } from 'react';
 
 const maxMETs = [10, 14, 18];
 const restingHeartRates = [100, 70, 50]
 
-const runningData = [
-    {
-        distance: 1.5,      // Distance ran in miles
-        duration: 0.25,      // hours
-        steps: 2500,
-        avgHeartRate: 125,
-        maxHeartRate: 145,
-        bodyWeight: 70,     // in Kg, equal to 154 lbs
-        fitnessLevel: 0
-    },
-    {
-        distance: 2,      
-        duration: 0.3,      
-        steps: 3200,
-        avgHeartRate: 130,
-        maxHeartRate: 150,
-        bodyWeight: 70,     
-        fitnessLevel: 0
-    },
-    {
-        distance: 2.5,      
-        duration: 0.35,      
-        steps: 4000,
-        avgHeartRate: 130,
-        maxHeartRate: 155,
-        bodyWeight: 70,     
-        fitnessLevel: 1
-    },
-    {
-        distance: 4,      
-        duration: 0.5,      
-        steps: 5500,
-        avgHeartRate: 125,
-        maxHeartRate: 145,
-        bodyWeight: 70,     
-        fitnessLevel: 1
-    },
-    {
-        distance: 1.5,      
-        duration: 0.5,      
-        steps: 4500,
-        avgHeartRate: 125,
-        maxHeartRate: 145,
-        bodyWeight: 70,     
-        fitnessLevel: 0
-    }
-]
-
-const distance = runningData.map(data => data.distance);
-const duration = runningData.map(data => data.duration);
-const steps = runningData.map(data => data.steps);
-const avgHeartRate = runningData.map(data => data.avgHeartRate);
-const maxHeartRate = runningData.map(data => data.maxHeartRate);
-const bodyWeight = runningData.map(data => data.bodyWeight);
-const fitnessLevel = runningData.map(data => data.fitnessLevel);
-
-// speed = distance / duration
-const speed = distance.map((data, index) => data / duration[index]);
-// pace = duration / distance
-// How long does it take to run a mile (or km if we decide to support that unit)
-const pace = duration.map((data, index) => data / distance[index]);
-// cadence = steps / minutes
-// Also known as steps per minute (SPM). Lower cadences (<160 SPM) indicates longer strides, and
-// more impact per step. This increases injury risk for beginners, so it is safer and more efficient
-// to have a cadence of 170 - 190 SPM
-const cadence = steps.map((data, index) => data / (duration[index] * 60));
-
-const heartRateReserve = avgHeartRate.map((data, index) => 
-    (data - restingHeartRates[fitnessLevel[index]]) / (maxHeartRate[index] - restingHeartRates[fitnessLevel[index]]));
-
-const MET = heartRateReserve.map((data, index) => (data * (maxMETs[fitnessLevel[index]] - 1) + 1))
-
-const caloriesBurned = MET.map((data, index) => parseInt(data * duration[index] * bodyWeight[index]));
-
-const labels = runningData.map((data, index) => `run ${index + 1}`)
-const graphMargin = 3;
-
 function RunningMetrics() {
+    const [editingData, setEditingData] = useState(false);
+    const [distanceIn, setDistanceIn] = useState(0);
+    const [durationIn, setDurationIn] = useState(0);
+    const [stepsIn, setStepsIn] = useState(0);
+    const [avgHeartRateIn, setAvgHeartRateIn] = useState(0);
+    const [maxHeartRateIn, setMaxHeartRateIn] = useState(0);
+    const [bodyWeightIn, setBodyWeightIn] = useState(0);
+    const [fitnessLevelIn, setFitnessLevelIn] = useState(0);
+    const [runningData, setRunningData] = useState([]);
+
+    const distance = runningData.map(data => data.distance);
+    const duration = runningData.map(data => data.duration);
+    const steps = runningData.map(data => data.steps);
+    const avgHeartRate = runningData.map(data => data.avgHeartRate);
+    const maxHeartRate = runningData.map(data => data.maxHeartRate);
+    const bodyWeight = runningData.map(data => data.bodyWeight);
+    const fitnessLevel = runningData.map(data => data.fitnessLevel);
+
+    // speed = distance / duration
+    const speed = distance.map((data, index) => data / duration[index]);
+    // pace = duration / distance
+    // How long does it take to run a mile (or km if we decide to support that unit)
+    const pace = duration.map((data, index) => data / distance[index]);
+    // cadence = steps / minutes
+    // Also known as steps per minute (SPM). Lower cadences (<160 SPM) indicates longer strides, and
+    // more impact per step. This increases injury risk for beginners, so it is safer and more efficient
+    // to have a cadence of 170 - 190 SPM
+    const cadence = steps.map((data, index) => data / (duration[index] * 60));
+
+    const heartRateReserve = avgHeartRate.map((data, index) => 
+        (data - restingHeartRates[fitnessLevel[index]]) / (maxHeartRate[index] - restingHeartRates[fitnessLevel[index]]));
+
+    const MET = heartRateReserve.map((data, index) => (data * (maxMETs[fitnessLevel[index]] - 1) + 1))
+
+    const caloriesBurned = MET.map((data, index) => parseInt(data * duration[index] * bodyWeight[index]));
+
+    const labels = runningData.map((data, index) => `run ${index + 1}`)
+    const graphMargin = 3;
+    const textInputSpacing = 3;
+
+    function handleEdit() {
+        editingData ? setEditingData(false) : setEditingData(true)
+    }
+
+
+    function handleSubmit() {
+        setRunningData(prevData => [
+            ...prevData,
+            {
+                distance: distanceIn,
+                duration: durationIn,
+                steps: stepsIn,
+                avgHeartRate: avgHeartRateIn,
+                maxHeartRate: maxHeartRateIn,
+                bodyWeight: bodyWeightIn,
+                fitnessLevel: fitnessLevelIn
+            }
+        ])
+    }
+
+    function handleReset() {
+        setRunningData([])
+    }
+    
+
+
     return (
         <Stack>
             <Typography fontSize={32}>
@@ -145,7 +135,91 @@ function RunningMetrics() {
                     />
                 </Card>
             </Stack>
-            <Link to="../fitnessTypes" className="button-link">Back to Fitness Types</Link>
+            <Stack>
+                {!editingData ? (
+                    <></>
+                ) : (
+                    <Card sx={{ padding:"40px", backgroundColor:"#828c85"}}>
+                        <Typography marginBottom={5} fontSize={24}>Input Running Metrics</Typography>
+                        <Stack direction="column" spacing={textInputSpacing}>
+                            <TextField 
+                                required
+                                variant="filled" 
+                                label="Distance"
+                                type="number"
+                                onChange={(e) => setDistanceIn(e.target.value)}
+                                InputProps={{ 
+                                    endAdornment: <InputAdornment position='end'>Miles</InputAdornment>
+                                 }}
+                            />
+                            <TextField 
+                                required 
+                                variant="filled" 
+                                label="Duration"
+                                type="number"
+                                onChange={(e) => setDurationIn(e.target.value)}
+                                InputProps={{ 
+                                    endAdornment: <InputAdornment position='end'>Hours</InputAdornment>
+                                 }}
+                            />
+                            <TextField 
+                                required 
+                                variant="filled" 
+                                label="Steps"
+                                type="number"
+                                onChange={(e) => setStepsIn(e.target.value)}
+                            />
+                            <TextField 
+                                required 
+                                variant="filled" 
+                                label="Average Heart Rate"
+                                type="number"
+                                onChange={(e) => setAvgHeartRateIn(e.target.value)}
+                            />
+                            <TextField 
+                                required 
+                                variant="filled" 
+                                label="Maximum Heart Rate"
+                                type="number"
+                                onChange={(e) => setMaxHeartRateIn(e.target.value)}
+                            />
+                            <TextField 
+                                required 
+                                variant="filled" 
+                                label="Bodyweight"
+                                type="number"
+                                onChange={(e) => setBodyWeightIn(e.target.value)}
+                                InputProps={{ 
+                                    endAdornment: <InputAdornment position='end'>Kg</InputAdornment>
+                                 }}
+                            />
+                            <TextField 
+                                required 
+                                variant="filled" 
+                                label="Fitness Level"
+                                type="number"
+                                onChange={(e) => setFitnessLevelIn(e.target.value)}
+                                InputProps={{ 
+                                    endAdornment: <InputAdornment position='end'>(0 - 2)</InputAdornment>
+                                 }}
+                            />
+                        </Stack>
+                        <Stack direction="row" justifyContent="center" spacing={5} marginTop={5}>
+                            <Button variant="contained" onClick={handleSubmit}>Submit</Button>
+                            <Button variant="contained" color="error" onClick={handleReset}>Reset Data</Button>
+                        </Stack>
+                    </Card>
+                )}
+            </Stack>
+            <Stack direction="row" marginTop={5} spacing={5} justifyContent="center">
+                <Button variant="contained" 
+                    onClick={handleEdit}
+                >
+                    {editingData ? "Stop Editing" : "Edit Data"}
+                </Button>
+                <Link to="../fitnessTypes" className="button-link">Back to Fitness Types</Link>
+            </Stack>
+            
         </Stack>
 
     );
