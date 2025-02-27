98282b609c8f5dfc79b5e4c4f0cbfc448ec717a0
NoJStein <127149391+NoJStein@users.noreply.github.com>
2025-02-26 16:46:01 -0800

    Added Input Validation to RunningMetrics
    Next I will add this same validation to all other metrics!
    
    This time my code is more documented if you are unsure of how this all works.

--- src/pages/RunningMetrics.jsx
+++ src/pages/RunningMetrics.jsx
@@ -9,15 +9,26 @@ const restingHeartRates = [100, 70, 50]
 function RunningMetrics() {
     const [editingData, setEditingData] = useState(false);
 
-    const [distanceIn, setDistanceIn] = useState(0);
-    const [durationIn, setDurationIn] = useState(0);
-    const [stepsIn, setStepsIn] = useState(0);
-    const [avgHeartRateIn, setAvgHeartRateIn] = useState(0);
-    const [maxHeartRateIn, setMaxHeartRateIn] = useState(0);
-    const [bodyWeightIn, setBodyWeightIn] = useState(0);
-    const [fitnessLevelIn, setFitnessLevelIn] = useState(0);
+    const [distanceIn, setDistanceIn] = useState(undefined);
+    const [durationIn, setDurationIn] = useState(undefined);
+    const [stepsIn, setStepsIn] = useState(undefined);
+    const [avgHeartRateIn, setAvgHeartRateIn] = useState(undefined);
+    const [maxHeartRateIn, setMaxHeartRateIn] = useState(undefined);
+    const [bodyWeightIn, setBodyWeightIn] = useState(undefined);
+    const [fitnessLevelIn, setFitnessLevelIn] = useState(undefined);
     
     const [runningData, setRunningData] = useState([]);
+
+    // This new state is accessed by the error attribute for each Textfield
+    const [errors, setErrors] = useState({
+        distance: false,
+        duration: false,
+        steps: false,
+        avgHeartRate: false,
+        maxHeartRate: false,
+        bodyWeight: false,
+        fitnessLevel: false
+    })
 
     const distance = runningData.map(data => data.distance);
     const duration = runningData.map(data => data.duration);
@@ -53,24 +64,61 @@ function RunningMetrics() {
         editingData ? setEditingData(false) : setEditingData(true)
     }
 
-
-    function handleSubmit() {
-        setRunningData(prevData => [
-            ...prevData,
-            {
-                distance: distanceIn,
-                duration: durationIn,
-                steps: stepsIn,
-                avgHeartRate: avgHeartRateIn,
-                maxHeartRate: maxHeartRateIn,
-                bodyWeight: bodyWeightIn,
-                fitnessLevel: fitnessLevelIn
-            }
-        ])
-    }
-
     function handleReset() {
         setRunningData([])
+    }
+
+    function handleSubmit() {
+        if (!isError()) {
+            setRunningData(prevData => [
+                ...prevData,
+                {
+                    distance: distanceIn,
+                    duration: durationIn,
+                    steps: stepsIn,
+                    avgHeartRate: avgHeartRateIn,
+                    maxHeartRate: maxHeartRateIn,
+                    bodyWeight: bodyWeightIn,
+                    fitnessLevel: fitnessLevelIn
+                }
+            ])
+        }
+    }
+
+    // Checking if text field input is either undefined, less than 1, or greater than the specified range
+    // If an error is found, this function returns true, preventing handleSubmit from storing the erroneous data
+    // in state.
+    function isError() {
+        // Making a new object with values equal to whether or not the input state meets certain conditions
+        // Will result in an object that holds Boolean values
+        let newErrors = {
+            distance: (distanceIn === undefined || distanceIn < 1),
+            duration: (durationIn === undefined || durationIn < 1),
+            steps: (stepsIn === undefined || stepsIn < 1),
+            avgHeartRate: (avgHeartRateIn === undefined || avgHeartRateIn < 1),
+            maxHeartRate: (maxHeartRateIn === undefined || maxHeartRateIn < 1),
+            bodyWeight: (bodyWeightIn === undefined || bodyWeightIn < 1),
+            fitnessLevel: (fitnessLevelIn === undefined || fitnessLevelIn < 0 || fitnessLevelIn > 2),
+
+        }
+
+        // Since we now have state for errors, we set it equal to the the values found within the object newErrors
+        // If any of the above conditions are true, the corresponding text field will enter an error state, indicating
+        // to the user that they entered erroneous input
+        setErrors(newErrors);
+
+        // Converting the newErrors object to a Boolean array of just the values from the newErrors object
+        let newErrorsArray = Object.values(newErrors)
+        let errorFound = false;
+
+        // Loop through the new Boolean array, setting errorFound to true if any values are true
+        newErrorsArray.forEach(val => {
+            if (val === true) {
+                errorFound = true;
+            }
+        })
+
+        return errorFound;
     }
     
 
@@ -148,26 +196,29 @@ function RunningMetrics() {
                                 variant="filled" 
                                 label="Distance"
                                 type="number"
+                                error={errors.distance}
                                 onChange={(e) => setDistanceIn(e.target.value)}
                                 InputProps={{ 
                                     endAdornment: <InputAdornment position='end'>Miles</InputAdornment>
-                                 }}
+                                }}
                             />
                             <TextField 
                                 required 
                                 variant="filled" 
                                 label="Duration"
                                 type="number"
+                                error={errors.duration}
                                 onChange={(e) => setDurationIn(e.target.value)}
                                 InputProps={{ 
                                     endAdornment: <InputAdornment position='end'>Hours</InputAdornment>
-                                 }}
+                                }}
                             />
                             <TextField 
                                 required 
                                 variant="filled" 
                                 label="Steps"
                                 type="number"
+                                error={errors.steps}
                                 onChange={(e) => setStepsIn(e.target.value)}
                             />
                             <TextField 
@@ -175,6 +226,7 @@ function RunningMetrics() {
                                 variant="filled" 
                                 label="Average Heart Rate"
                                 type="number"
+                                error={errors.avgHeartRate}
                                 onChange={(e) => setAvgHeartRateIn(e.target.value)}
                             />
                             <TextField 
@@ -182,6 +234,7 @@ function RunningMetrics() {
                                 variant="filled" 
                                 label="Maximum Heart Rate"
                                 type="number"
+                                error={errors.maxHeartRate}
                                 onChange={(e) => setMaxHeartRateIn(e.target.value)}
                             />
                             <TextField 
@@ -189,20 +242,22 @@ function RunningMetrics() {
                                 variant="filled" 
                                 label="Bodyweight"
                                 type="number"
+                                error={errors.bodyWeight}
                                 onChange={(e) => setBodyWeightIn(e.target.value)}
                                 InputProps={{ 
                                     endAdornment: <InputAdornment position='end'>Kg</InputAdornment>
-                                 }}
+                                }}
                             />
                             <TextField 
                                 required 
                                 variant="filled" 
                                 label="Fitness Level"
                                 type="number"
+                                error={errors.fitnessLevel}
                                 onChange={(e) => setFitnessLevelIn(e.target.value)}
                                 InputProps={{ 
                                     endAdornment: <InputAdornment position='end'>(0 - 2)</InputAdornment>
-                                 }}
+                                }}
                             />
                         </Stack>
                         <Stack direction="row" justifyContent="center" spacing={5} marginTop={5}>
