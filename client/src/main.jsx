e72a33bb440dbee22cb756a40a271dc069f298a2
NoJStein <127149391+NoJStein@users.noreply.github.com>
2025-04-08 16:40:01 -0700

    Now your system preferences determine the theme
    This is documented quite well so I would read the comments if you are curious.
    
    This is leading me to consider if we really should be storing the user's theme preference in our DB. A toggle is fun and all, but I (as a dark mode enjoyer) never want to see a webpage or application blind me with a light mode theme. Thus why I set my system preferences to dark mode to begin with.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,17 +3,36 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, useMediaQuery } from '@mui/material';
 
 import DarkTheme from '../theme/DarkTheme.jsx';
+import LightTheme from '../theme/LightTheme.jsx';
+
+
+// I needed the functionality of useMediaQuery to figure out the system
+// preference for light/dark mode. However, it uses the context hook and
+// the rules of react state that all hooks need to be placed within
+// components. So I made one (ThemedApp) that will return our app
+// wrapped in the theme that the below logic determines.
+// I didn't realize you could do this!
+const ThemedApp = () => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const chosenTheme = prefersDarkMode ? DarkTheme : LightTheme;
+
+  return (
+    <ThemeProvider theme={chosenTheme}>
+      <CssBaseline disableColorScheme />
+      <App />
+    </ThemeProvider>
+
+  );
+
+}
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-      <ThemeProvider theme={DarkTheme}>
-        <CssBaseline disableColorScheme />
-        <App />
-      </ThemeProvider>
-      
+      <ThemedApp/>
     </StrictMode>
 
 )
