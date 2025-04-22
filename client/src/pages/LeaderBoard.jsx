9ec1b16f20af84e489ffd26440fc32baf96b080f
NoJStein <127149391+NoJStein@users.noreply.github.com>
2025-04-22 16:18:42 -0700

    All user exercise data has been fed in and formatted
    I'm going for a clean slate with this version of the leaderboard page, so I have deleted everything and starting fresh because this is one of our oldest pages.
    
    All the new code you see here formats user metrics very similarly to how they are formatted on the profile page for determining which achievements a user has earned. The format is a JSON object that will look something like this:
    object = {
        distance: {run: 90.3, cycle: 5, hike: 10}
        duration: {run: 16.150000000000002, cycle: 0.75, hike: 2}
        elevationGain: {cycle: 250, hike: 500}
        elevationLoss: 0
        lapCount: 8
        maxHeartRate: 141
        maxWeightOfWeights: 8
        name: "Crow"
        steps: 121200
        totalLapTime: 355
        totalReps: 270
        totalStrokes: 441
        totalVolume: 1710
    }
    
    I have an array of these objects, so hopefully I can form a meaningful leaderboard with this data.

--- client/src/pages/LeaderBoard.jsx
+++ client/src/pages/LeaderBoard.jsx
@@ -1,160 +1,186 @@
-import React, {use, useState} from 'react';
 import { useNavigate, Link  } from 'react-router-dom';
 import { Stack, Avatar, Button, ButtonGroup, Typography} from '@mui/material';
 import { amber, blueGrey, orange } from '@mui/material/colors';
 import ToolBar from '../components/ToolBar';
-
+import GlobalStateContext from '../contexts/GlobalStateContext';
+import { useContext, useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link as RouterLink } from 'react-router-dom';
 import MuiLink from '@mui/material/Link';
 
-// The arrays for testing that will be replaced when we get user data proper
-var userName = ["Abel", "Bender", "Chirst", "Don", "Eve"];
-var userDistance = [1.5, 2, 2.5, 4, 1];
-var userMaxHeartRate = [145, 150, 155, 135, 115];
-var userDuration = [0.5, 0.25, 0.3, 0.35, 0.5];
-
-// The arrays needed to store data that will be displayed later
-var leaders = ["1", "2", "3", "4", "5"];
-var leaderData = ["NULL", "NULL", "NULL", "NULL", "NULL"];
-var maxMinMedian = ["max", "min", "median"];
-
-
-function handleButtons(buttonNum, setLeaderType, SetName, SetValues, setMeasure, setMaxMinMedian){
-  // uses a switch state that takes the number value of the button that was used then sets display data accordingly
-  // when setting the display values it uses the arrays at the top and copies them to the html arrays
-  var dataType = "Sorted by ";
-  var unitsOfMeasurement = "";
-  switch (buttonNum){
-    case 1:
-      sortData(userDistance, userName);
-      dataType += "Distance";
-      unitsOfMeasurement = " mi"
-      break;
-    case 2:
-      sortData(userMaxHeartRate, userName);
-      dataType += "MaxHeartRate";
-      unitsOfMeasurement = " bpm"
-      break;
-    case 3:
-      sortData(userDuration, userName);
-      dataType += "Duration";
-      unitsOfMeasurement = "h";
-      break;
-  }
-  setLeaderType(dataType + ":");
-  SetName(leaders.slice(0, 5));
-  SetValues(leaderData.slice(0, 5));
-  setMeasure(unitsOfMeasurement)
-  setMaxMinMedian(maxMinMedian.slice(0, 3));
-}
-
-function makeLeaders(exerciseData, userData){
-// currectly only displays the largest value at the top can be fixed with suggetion below
-// exerciseData[i] < exerciseData[i+1] then display in ascending or descending order
-// stores the data in leaders, used for the names, and leaderData, used for the values
-let n = userData.length;
-leaders = userData.slice(0, n);
-leaderData = exerciseData.slice(0, n);
-for (let i = 1; i <= n; i++){
-  leaders[i-1] = userData[n-i];
-  leaderData[i-1] = exerciseData[n-i];
-}
-}
-
-function findMaxMinMedian(sortedData){
-  //gets the highest lowest and middle value (in that order) from the sorted data
-  // then stores them in the array maxMinMedian which will be in handle buttons
-  var lengthOfArray = sortedData.length;
-  if (sortedData[0] > sortedData[lengthOfArray-1]){
-    maxMinMedian[0] = sortedData[0];
-    maxMinMedian[1] = sortedData[lengthOfArray-1];
-  }
-  else {
-    maxMinMedian[0] = sortedData[lengthOfArray-1];
-    maxMinMedian[1] = sortedData[0];
-  }
-  if (lengthOfArray%2 == 0) {
-    maxMinMedian[2] = sortedData[lengthOfArray/2];
-  }
-  else {
-    maxMinMedian[2] = sortedData[(lengthOfArray/2)-.5];
-  }
-}
-
-function sortData(exerciseData, userData){
-//bubble sort
-//calls makeLeaders and findMaxMinMedian so they can use the sorted data
-let n = exerciseData.length; 
-let swapped = true;
-let temp = exerciseData[0];
-let userTemp = userData[0];
-let sortedData = exerciseData.slice(0, n);
-let sortedUser = userData.slice(0, n);
-
-while(swapped){
-  swapped = false
-  JSON.stringify(temp)
-  for(let i = 1; i < n; i++){
-  if (sortedData[i-1] > sortedData[i]){
-      temp = sortedData[i - 1];
-      sortedData[i - 1] = sortedData[i];
-      sortedData[i] = temp;
-
-      userTemp = sortedUser[i - 1];
-      sortedUser[i - 1] = sortedUser[i];
-      sortedUser[i] = userTemp;
-
-      swapped = true;
-  }
-  }
-  n = n - 1;
-}
-makeLeaders(sortedData, sortedUser);
-findMaxMinMedian(sortedData)
-}
 
 function LeaderBoard () {
-  // arrays that store the dayplay data from the javascript, There might be another way to do this but I can't html good
-  const navigate = useNavigate();
-  const [message, setMessage] = useState('Please Click a Button');
-  const [placeNames, setPlaces] = useState(["1st", "2nd", "3rd", "4th", "5th"]);
-  const [placeValues, setValues] = useState(["XX", "XX", "XX", "XX", "XX"]);
-  const [unitsOfMeasure, setMeasure] = useState([""]);
-  const [maxMinMedian, setMMM] = useState(["__", "__", "__"])
 
-  // most of the is pretty easy to read, but just in case
-  // its starts with header of Leaderboard then the button which call handleButtons when clicked on
-  // in handleButtons we pass in the arrays that will be used to display data later
-  // after that we have the avaters of 1st 2nd and 3rd in that order, something to be expanded apon later
-  // after that we have the top 5 users with their exercise value and unit of measurement for said value
-  // lastly we have the button to return to the main page
+  const { state, dispatch } = useContext(GlobalStateContext)
+  const [userData, setUserData] = useState([]);
+  // very similar to userData except it will hold the cumulative exercise data
+  // for each user.
+  const [userExercises, setUserExercises] = useState([]);
+  const [cumulativeMetrics, setCumulativeMetrics] = useState([]);
+
+
+  // Fetching every user from the database
+  async function getUserData() {
+    try {
+        const res = await axios.get('http://localhost:3000/users', {
+            headers: {
+                'Content-Type': 'application/json'
+            }, 
+            params: { }
+        });
+        return res.data
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// Fetching all the exercises that are associated with a userID
+async function getUserExercises(id) {
+  try {
+    const res = await axios.get('http://localhost:3000/exercises', {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      params: {
+        userID: id
+      }
+    });
+      return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// When page loads, fetch all user profile data
+useEffect(() => {
+  async function fetchData() {
+    const response = await getUserData();
+    setUserData(response);
+  }
+
+  // Calling the function above
+  fetchData();
+
+}, [])
+
+// When userData is assigned a value, fetch an array of all exercises associated
+// with a user, but for all users.
+// We want a list of which users have done which exercises
+useEffect(() => {
+  async function fetchExercises() {
+    if (userData.length === 0) return;
+
+    // The result of this sequence of API requests is stored in metricsArray
+    // We make sure that all async promises are fulfilled within metricsArray
+    // before moving on.
+    const metricsArray = await Promise.all(
+      userData.map(async (user, idx) => {
+        const userExercises = await getUserExercises(user._id);
+        return { name: user.name, metrics: userExercises };  
+      })
+    )
+
+    // set user metrics to our newly-fulfilled array of user exercises
+    setUserExercises(metricsArray)
+  }
+
+  // Calling the function above
+  fetchExercises();
+}, [userData])
+
+
+  // This function goes through our list of exercises per user and creates an
+  // object (metric) that holds an accumulation of all relevant stats that we
+  // can use to feed into a leaderboard. 
+  async function sumAllUserMetrics() {
+
+    let allMetrics = [];
+
+    userExercises.map((user, idx) => {
+
+      let metric = {
+        name: user.name,
+        distance: {
+            run: 0,
+            cycle: 0,
+            hike: 0
+        },
+        duration: {
+            run: 0,
+            cycle: 0,
+            hike: 0
+        },
+        elevationGain: {
+            cycle: 0,
+            hike: 0
+        },
+        elevationLoss: 0,
+        maxHeartRate: 1,
+        steps: 0,
+        maxWeightOfWeights: 0,
+        totalVolume: 0,
+        totalReps: 0,
+        lapCount: 0,
+        totalLapTime: 0,
+        totalStrokes: 0
+      }
+
+      user.metrics.map((data, idx) => {
+
+        if (data.type === "run") {
+            metric.distance.run += data.distance;
+            metric.duration.run += data.duration;
+            metric.steps += data.steps;
+        } else if (data.type === "hike") {
+            metric.elevationGain.hike += data.elevationGain;
+            metric.distance.hike += data.distance;
+            metric.duration.hike += data.duration;
+        } else if (data.type === "cycle") {
+            metric.distance.cycle += data.distance;
+            metric.duration.cycle += data.duration;
+            metric.elevationGain.cycle += data.elevationGain;
+        } else if (data.type === "swim") {
+            metric.lapCount += data.lapCount;
+            data.lapTimes.map((data, idx) => {
+                metric.totalLapTime += data;
+            })
+            data.strokeCount.map((data, idx) => {
+                metric.totalStrokes += data
+            })
+        } else if (data.type === "weights") {
+            metric.totalReps += (data.reps * data.sets);
+            if (metric.maxWeightOfWeights < data.weightOfWeights) {
+                metric.maxWeightOfWeights = data.weightOfWeights
+            }
+            metric.totalVolume += (data.reps * data.sets * data.weightOfWeights);
+
+        }
+
+        if (metric.maxHeartRate < data.maxHeartRate) {
+            metric.maxHeartRate = data.maxHeartRate;
+        }
+      })
+
+      allMetrics.push(metric);
+    })
+
+    setCumulativeMetrics(allMetrics);
+  }
+
+  // When all user exercises are fetched, run the function above to accumulate
+  // their metrics. 
+  useEffect(() => {
+    if (userExercises.length === 0) return;
+
+    sumAllUserMetrics();
+    console.log("3rd hook cumulativeMetrics: ", cumulativeMetrics)
+  }, [userExercises])
+
+
   return (
     <>
       <ToolBar /> {/* add new elements */}
-      <Stack direction="column" gap={2} marginBottom={3}>
-        <Typography fontSize={36}>Leaderboard</Typography>
 
-        <ButtonGroup variant="contained" aria-label="Basic button group">
-          <Button onClick={() => handleButtons(1, setMessage, setPlaces, setValues, setMeasure, setMMM)}>Distance</Button>
-          <Button onClick={() => handleButtons(2, setMessage, setPlaces, setValues, setMeasure, setMMM)}>MaxHeartRate</Button>
-          <Button onClick={() => handleButtons(3, setMessage, setPlaces, setValues, setMeasure, setMMM)}>Duration</Button>
-        </ButtonGroup>
-        <Stack direction="row" gap={2} justifyContent={"center"}>
-          <Avatar sx={{ bgcolor: amber[600] }} variant="square">{placeNames[0][0]}</Avatar>
-          <Avatar sx={{ bgcolor: blueGrey[200] }} variant="square">{placeNames[1][0]}</Avatar>
-          <Avatar sx={{ bgcolor: orange[500] }} variant="square">{placeNames[2][0]}</Avatar>
-        </Stack>
-        <Typography fontSize={20}>
-        {message}
-        <br></br>{placeNames[0]} -- {placeValues[0]} {unitsOfMeasure}
-        <br></br>{placeNames[1]} -- {placeValues[1]} {unitsOfMeasure}
-        <br></br>{placeNames[2]} -- {placeValues[2]} {unitsOfMeasure}
-        <br></br>{placeNames[3]} -- {placeValues[3]} {unitsOfMeasure}
-        <br></br>{placeNames[4]} -- {placeValues[4]} {unitsOfMeasure}
-        </Typography>
-        <Typography fontSize={20}>Max:{maxMinMedian[0]}  Min:{maxMinMedian[1]}  Median:{maxMinMedian[2]}</Typography>
-        <MuiLink to="../HomePage" component={RouterLink}>Back to Home</MuiLink>
-      </Stack>
     </>
   );
 }
