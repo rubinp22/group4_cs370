fb7883995cca8882e875f830e740a2c9a234551f
rubinp22 <rubinp@sonoma.edu>
2025-04-24 20:07:30 -0700

    Toolbar menu dropdown
    Included toolbar icon with a dropdown list. Which holds sign out and a my profile feature.

--- client/src/components/ToolBar.jsx
+++ client/src/components/ToolBar.jsx
@@ -1,10 +1,30 @@
-import { useContext } from "react";
-import { Typography, AppBar, Toolbar, IconButton, Box, Avatar } from "@mui/material";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Typography, AppBar, Toolbar, IconButton, Box, Avatar, Menu, MenuItem } from "@mui/material";
 import GlobalStateContext from "../contexts/GlobalStateContext";
-
 
 function ToolBar() {
   const { state } = useContext(GlobalStateContext);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const navigate = useNavigate();
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleProfileClick = () => {
+    handleMenuClose();
+    navigate(`./profile/${state.user}`);
+  };
+
+  const handleLogoutClick = () => {
+    handleMenuClose();
+    navigate("../");
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -16,13 +36,29 @@ function ToolBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={handleMenuOpen}
           >
-            {/* Optional icon */}
+            ☰
           </IconButton>
+
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Fitness Tracker
           </Typography>
           <Avatar src={state.pfp} />
+
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+            sx={{
+              "& .MuiPaper-root": {
+                minWidth: 230, 
+              }
+            }}
+          >
+            <MenuItem onClick={handleProfileClick}>Profile</MenuItem>
+            <MenuItem onClick={handleLogoutClick}>Logout</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
     </Box>
