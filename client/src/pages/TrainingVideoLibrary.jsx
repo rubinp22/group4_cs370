07a33937a2080f8e5b4fcf5fb00389a34dd7acf7
NoJStein <127149391+NoJStein@users.noreply.github.com>
2025-04-28 20:33:39 -0700

    Removed the "All Videos" button
    It turns out that loading 50 embedded YouTube videos (10 categories times 5 videos per category) is REALLY CPU intensive! Also scrolling through 50 videos to find the category/video you want is harder than filtering by category.

--- client/src/pages/TrainingVideoLibrary.jsx
+++ client/src/pages/TrainingVideoLibrary.jsx
@@ -28,7 +28,7 @@ const videoLibrary = {
       { title: 'Yoga Video 2', url: 'https://www.youtube.com/watch?v=2WE-L8iyu0U' },
       { title: 'Yoga Video 3', url: 'https://www.youtube.com/watch?v=Y9CO4lliOow' },
       { title: 'Yoga Video 4', url: 'https://www.youtube.com/watch?v=Qy3U09CnELI' },
-      { title: 'Yoga Video 5', url: 'https://www.youtube.com/watch?v=nDJAbRkjDuA' }
+      { title: 'Yoga Video 5', url: 'https://www.youtub e.com/watch?v=nDJAbRkjDuA' }
   ],
   pilates: [
       { title: 'Pilates Video 1', url: 'https://www.youtube.com/watch?v=ofa5hlozkMs' },
@@ -79,58 +79,6 @@ const videoLibrary = {
       { title: 'Lifting Video 4', url: 'https://www.youtube.com/watch?v=Yb2TgKMA8y4' },
       { title: 'Lifting Video 5', url: 'https://www.youtube.com/watch?v=BNsKEG3hIzI' }
   ],
-  all: [
-      { title: 'Strength Video 1', url: 'https://www.youtube.com/watch?v=ZCcX2Egirp4' },
-      { title: 'Strength Video 2', url: 'https://www.youtube.com/watch?v=9gjJb09eICo' },
-      { title: 'Strength Video 3', url: 'https://www.youtube.com/watch?v=l5ij5MJpVP0' },
-      { title: 'Strength Video 4', url: 'https://www.youtube.com/watch?v=eGHftDN0Eyc' },
-      { title: 'Strength Video 5', url: 'https://www.youtube.com/watch?app=desktop&v=rNRtI_LOA0Y' },
-      { title: 'Cardio Video 1', url: 'https://www.youtube.com/watch?v=q2NZyW5EP5A' },
-      { title: 'Cardio Video 2', url: 'https://www.youtube.com/watch?v=YraWLoZH-QQ' },
-      { title: 'Cardio Video 3', url: 'https://www.youtube.com/watch?v=gUWFmn8f3H4' },
-      { title: 'Cardio Video 4', url: 'https://www.youtube.com/watch?v=bYO8V-6IFs0' },
-      { title: 'Cardio Video 5', url: 'https://www.youtube.com/watch?v=nLJ503CinLQ' },
-      { title: 'Yoga Video 1', url: 'https://www.youtube.com/watch?v=Kvoq4luIYVc' },
-      { title: 'Yoga Video 2', url: 'https://www.youtube.com/watch?v=2WE-L8iyu0U' },
-      { title: 'Yoga Video 3', url: 'https://www.youtube.com/watch?v=Y9CO4lliOow' },
-      { title: 'Yoga Video 4', url: 'https://www.youtube.com/watch?v=Qy3U09CnELI' },
-      { title: 'Yoga Video 5', url: 'https://www.youtube.com/watch?v=nDJAbRkjDuA' },
-      { title: 'Pilates Video 1', url: 'https://www.youtube.com/watch?v=ofa5hlozkMs' },
-      { title: 'Pilates Video 2', url: 'https://www.youtube.com/watch?v=UIKUTS_3K98' },
-      { title: 'Pilates Video 3', url: 'https://www.youtube.com/watch?v=5H8K9HL9QaU' },
-      { title: 'Pilates Video 4', url: 'https://www.youtube.com/watch?v=bVcvQW0XzOs' },
-      { title: 'Pilates Video 5', url: 'https://www.youtube.com/watch?v=b8ueXAFnBf0' },
-      { title: 'Dance Video 1', url: 'https://www.youtube.com/watch?v=uKEPTDLgSWs' },
-      { title: 'Dance Video 2', url: 'https://www.youtube.com/watch?v=zeDhQq6s-Ws' },
-      { title: 'Dance Video 3', url: 'https://www.youtube.com/watch?v=so0eQJVtZEM' },
-      { title: 'Dance Video 4', url: 'https://www.youtube.com/watch?v=QKV-KhMjOWA' },
-      { title: 'Dance Video 5', url: 'https://www.youtube.com/watch?v=jRLvEQsguMk' },
-      { title: 'Running Video 1', url: 'https://www.youtube.com/watch?v=u-e0ZO5L0s0' },
-      { title: 'Running Video 2', url: 'https://www.youtube.com/watch?v=RCBOV43EDg0' },
-      { title: 'Running Video 3', url: 'https://www.youtube.com/watch?v=_kGESn8ArrU' },
-      { title: 'Running Video 4', url: 'https://www.youtube.com/watch?v=LqTCmMtWdGA' },
-      { title: 'Running Video 5', url: 'https://www.youtube.com/watch?v=utADwnhrCQ4' },
-      { title: 'Cycling Video 1', url: 'https://www.youtube.com/watch?v=4ssLDk1eX9w' },
-      { title: 'Cycling Video 2', url: 'https://www.youtube.com/watch?v=gUapMtJIb1s' },
-      { title: 'Cycling Video 3', url: 'https://www.youtube.com/watch?v=M-viV5DZ2mo' },
-      { title: 'Cycling Video 4', url: 'https://www.youtube.com/watch?v=bb9m_n4jckQ' },
-      { title: 'Cycling Video 5', url: 'https://www.youtube.com/watch?v=VKBNDzCLzK8' },
-      { title: 'Hiking Video 1', url: 'https://www.youtube.com/watch?v=lrESF9eecxk' },
-      { title: 'Hiking Video 2', url: 'https://www.youtube.com/watch?v=Ul9ryQiK8VM' },
-      { title: 'Hiking Video 3', url: 'https://www.youtube.com/watch?v=ZnOzpERDmjU' },
-      { title: 'Hiking Video 4', url: 'https://www.youtube.com/watch?v=PDJJaPxCbxg' },
-      { title: 'Hiking Video 5', url: 'https://www.youtube.com/watch?v=YXjyrUd2mTg' },
-      { title: 'Swimming Video 1', url: 'https://www.youtube.com/watch?v=6_vXycbD2TM' },
-      { title: 'Swimming Video 2', url: 'https://www.youtube.com/watch?v=ZdKBXAoxoDE' },
-      { title: 'Swimming Video 3', url: 'https://www.youtube.com/watch?v=lFU0aZEElqc' },
-      { title: 'Swimming Video 4', url: 'https://www.youtube.com/watch?v=riIyImmuB_M' },
-      { title: 'Swimming Video 5', url: 'https://www.youtube.com/watch?v=WaKMgWGXE9c' },
-      { title: 'Lifting Video 1', url: 'https://www.youtube.com/watch?v=u7AFpzWV2I8' },
-      { title: 'Lifting Video 2', url: 'https://www.youtube.com/watch?v=dEKpR6sGxDY' },
-      { title: 'Lifting Video 3', url: 'https://www.youtube.com/watch?v=4Y2ZdHCOXok' },
-      { title: 'Lifting Video 4', url: 'https://www.youtube.com/watch?v=Yb2TgKMA8y4' },
-      { title: 'Lifting Video 5', url: 'https://www.youtube.com/watch?v=BNsKEG3hIzI' }
-  ],
   none: [{title: 'Select a button at the top to see exercise videos', url:'null'}]
 };
 
@@ -171,7 +119,6 @@ function TrainingVideoLibrary () {
         <Button onClick={() => setVideoType("yoga")}>yoga</Button>
         <Button onClick={() => setVideoType("pilates")}>pilates</Button>
         <Button onClick={() => setVideoType("dance")}>dance</Button>
-        <Button onClick={() => setVideoType("all")}>All Videos</Button>
       </ButtonGroup>
 
       {/* Mapping videos alonging with embedding them */}
